3b6e3e4df5f2e349a7ad50268ee6f55c6dab8a5f
Bruno S Velho <bruno.velho@gmail.com>
2023-10-13 16:36:09 -0300

    Projeto Limpo para iniciar desenvolvimento

--- src/App.jsx
+++ src/App.jsx
@@ -1,26 +1,8 @@
-import { CardAvatar } from "./components/Avatar"
-
-import style from "./App.module.css"
 
 function App() {
-
-  const listStack = [
-    { Tec: "JavaScript" },
-    { Tec: "HTML" },
-    { Tec: "CSS" },
-    { Tec: "Typescript" },
-    { Tec: "React" },
-  ];
-
   return (
-    <div className={style.container}>
-      <CardAvatar
-        cover="https://images.unsplash.com/photo-1605379399642-870262d3d051?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=350&q=50"
-        avatar="https://github.com/vbruno.png"
-        name="Bruno S Velho"
-        office="Web Developer"
-        stacks={listStack}
-      />
+    <div>
+      <h1>Projeto Limpo para iniciar desenvolvimento</h1>
     </div>
   )
 }
--- src/components/Stack/index.jsx
+++
@@ -1,3 +0,0 @@
-export function Stack({ name }) {
-  return <span>#{name}</span>;
-}
