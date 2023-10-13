a052adc81faceb6256a79efc5f210a9b8ab3a147
Bruno S Velho <bruno.velho@gmail.com>
2023-10-13 17:33:12 -0300

    Ajuste para adicionar TypeProps

--- src/components/Stack/index.jsx
+++ src/components/Stack/index.jsx
@@ -1,3 +1,13 @@
+import PropTypes from "prop-types";
+
 export function Stack({ name }) {
   return <span>#{name}</span>;
+}
+
+Stack.prototypes = {
+  name: PropTypes.string
+}
+
+Stack.defaultProps = {
+  name: "default",
 }
